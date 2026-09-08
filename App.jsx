ca5f950920424ad3fb63db5a327910151134b1d9
SheillaO <sheilla.alusa@strathmore.edu>
2026-09-08 11:27:22 +0300

    Run npm install

--- App.jsx
+++ App.jsx
@@ -1,1 +1,16 @@
 import Entry from "./components/Entry";
+import Header from "./components/Header";
+import data from "./data";
+
+const entryElement = data.map((entry) => (
+    <Entry key={entry.id} {...entry} />
+));
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            {entryElement}
+        </div>
+    );
+}
