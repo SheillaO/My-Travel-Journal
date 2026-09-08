9ce2267ad0cf9e3f815de350f4bf6c67344366cc
SheillaO <sheilla.alusa@strathmore.edu>
2026-09-08 12:06:49 +0300

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,7 +3,7 @@ import globe from "../images/globe.png";
 export default function Header() {
   return (
     <header>
-      <img src="../images/globe.png" alt="globe icon" />
+      <img src={globe} alt="A simple globe icon with latitude and longitude lines, centered in a clean white space, representing travel and global exploration; the design is minimal and friendly, with a calm, welcoming tone" />
       <h1>My Travel Journal</h1>
     </header>
   );
