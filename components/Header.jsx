325d3216e9486e66c7422006398631fda459cefa
SheillaO <sheilla.alusa@strathmore.edu>
2026-09-08 12:04:33 +0300

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,10 @@
+import globe from "../images/globe.png";
+
 export default function Header() {
   return (
     <header>
       <img src="/images/globe.png" alt="globe icon" />
       <h1>My Travel Journal</h1>
     </header>
-  )
+  );
 }
